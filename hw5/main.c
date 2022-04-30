820ee4c65a2209c0e56e6aa7f0eb9747ce7d4ba2
Chien <100185381+b1029009Chien@users.noreply.github.com>
2022-04-30 14:35:12 +0800

    Update main.c

--- hw5/main.c
+++ hw5/main.c
@@ -1,70 +1,74 @@
-#include<stdio.h>
+#include <stdio.h>
 
 typedef struct my_mm {
-    int nm[10];
-    int* nm_p[10];
-    int count;
+    int* calloced_p[10];
 }my_mm_t;
 
+int g_mm[10];
 my_mm_t mms;
-int g_mem[10];
 
-void mms_detect() {
-    for(int i = 0 ; i < 10 ; i++) {
-        if(g_mem[i]! = 0) mms.nm[i] = 1;
-        else mms.nm[i] = 0;
-        printf("%d",mms.nm[i]);
-    }    
+void printf_calloc_array(int flag) {
+    for(int i = 0;i<10;i++) {
+        (mms.calloced_p[i])?printf("1"):printf("0");
+    }
+    if(!flag) {
+        printf("<-out of space");
+    }
+    printf("\n");
 }
 
 int* my_calloc(int n, int size) {
-    mms.count++;
-    int np = 0;
-    for(int i = 0 ; i < 10 ; i++) {
-        if(g_mem[i] == 0) np++;
-        else np = 0;
-        if(np >= n*size) {
-            for(int j = i-np+1 ; j < i+1; j++) {
-                g_mem[j] = mms.count;
-            }            
-            mms_detect();
-            printf("\n");
-            return &g_mem[i];
+    int av_size = 0;
+    int index;
+    //find the index of array
+    for (index = 0;index<10;index++) {
+        //check array[index] i sfree or not
+        if(!mms.calloced_p[index]) {
+            av_size++;
+        }else {
+            av_size = 0;
+        }
+        //find
+        if (av_size == size) {
+            index -= size-1;
+            break;
         }
     }
-    mms_detect();
-    printf("<-Out of space\n");
-    return NULL;
+    // can not find the space
+    if(av_size < size) {
+        printf_calloc_array(0);
+        return NULL;
+    }
+    //alloced these spaces
+    for(int i = 0;i<size;i++) {
+        mms.calloced_p[index+i] = &g_mm[index];
+    }
+    //print out the mms
+    printf_calloc_array(1);
+    //return the address of array[index]
+    return &g_mm[index];
+} 
+
+void my_free(int* p) {
+    for(int i = 0; i<10; i++) {
+        if(mms.calloced_p[i] == p) {
+            mms.calloced_p[i] = NULL;
+        }
+    }
+    printf_calloc_array(1);
 }
 
-int* findAddress(int* n, int num) {
-    int count = 0;
-    while(*n! = num) {
-        n++;
-        count++;
-        if(count > 10) return 0;
-    }
-    return n;
-}
 
-void my_free(int* p) {                
-    int count = 0;
-    while(findAddress(g_mem, *p) && count < 10) {
-        int *n = findAddress(g_mem, *p);
-        *n = 0;
-        count++;
-    }
-    mms_detect();
-    printf("\n");
-}
-    
 int main() {
-    int* p1 = my_calloc(1,1);
-    int* p2 = my_calloc(1,2);
-    int* p3 = my_calloc(1,3);
-    int* p4 = my_calloc(1,4);    
-    my_free(p1);
-    my_free(p4);
-    int* p5 = my_calloc(1,5);
-    int* p6 = my_calloc(1,1);
+    int* np1 = my_calloc(1, 1);
+    int* np2 = my_calloc(1, 2);
+    int* np3 = my_calloc(1, 3);
+    int* np4 = my_calloc(1, 4);
+    my_free(np1);
+    my_free(np4);
+    int* np5 = my_calloc(1, 5);
+    int* np6 = my_calloc(1, 3);
+
+    return 0;
 }
+
