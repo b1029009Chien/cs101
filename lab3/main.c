f45cff222ba2833325f50be4949daf91eb43d05b
Chien <100185381+b1029009Chien@users.noreply.github.com>
2022-04-30 14:49:10 +0800

    Update main.c

--- lab3/main.c
+++ lab3/main.c
@@ -6,20 +6,29 @@ int my_strlen(char* s) {
     }
 }
 
-int rec_strlen(char* s,int i) {
-    if (i==0) return 1;
-    return rec_strlen(i+1) ;
+int rec_strlen(char* s) {
+    if (!*s) return 0;
+    return 1 + my_strlen(s+1) ;
 }
 
 char* my_sort(char* s) {
-    
+    for(char* t = s; *t; t++) {
+        for(char* p = t+1; *p;p++) {
+            if(*t > *p) {
+                char tmp = *t;
+                *t = *p;
+                *p = tmp;
+            }
+        }
+    }
+    return s;
 }
 
 int main() {
     char s[] = "IU is a girl!";
     printf("len = %d\n", my_strlen(s));
     printf("len = %d\n", rec_strlen(s));
-    
+    printf("sort = %s\n", my_sort(s));
 
     return 0;
 }
