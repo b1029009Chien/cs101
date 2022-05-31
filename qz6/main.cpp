678a6d44a4290330669a23dc5c5b54a5a7d00319
Chien <100185381+b1029009Chien@users.noreply.github.com>
2022-05-31 14:48:25 +0800

    Create main.cpp

---
+++ qz6/main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Clock {
+    protected:
+        int Hour;
+        int minute;
+        int second;
+        string meridian;
+    public:
+        Clock(int x, int y, int z, string w) {
+            if(x >= 1 && x <= 12)Hour = x;
+            if(y >= 0 && y <= 59)minute = y;
+            if(z >= 0 && z <= 59)second = z;
+            if(!w.compare("AM")||!w.compare("PM"))meridian = w;
+        }
+        void display() {
+            cout << Hour << ":" << minute << ":" << second << meridian <<endl;
+        }
+};
+
+class StandardClock : Clock {
+    public:
+        StandardClock(int x, int y, int z, string w) : Clock(x, y, z, w) {
+            Hour = x;
+            minute = y;
+            second = z;
+            meridian = w;
+        }
+        void display() {
+            if (meridian[0] == 'A') {
+                cout << "上午" << Hour << ":" << minute << ":" << second << endl;
+            }
+            if (meridian[0] == 'P') {
+                cout << "下午" << Hour << ":" << minute << ":" << second << endl;
+            }
+        }
+};
+
+class MilitaryClock : Clock {
+    public:
+        MilitaryClock(int x, int y , int z, string w) : Clock(x, y, z, w) {
+            Hour = x;
+            minute = y;
+            second = z;
+            meridian = w;
+        }
+        void display() {
+            if (!meridian.compare("AM"))
+                cout << Hour;
+            if (!meridian.compare("PM")) {
+                cout << Hour+12;
+            cout<< ":" << minute << ":" << second << endl;
+            }
+        }
+};
+
+int main() {
+    Clock cc(10, 25, 43, "AM");
+    StandardClock sc(10, 25, 43, "AM");
+    MilitaryClock mc(10, 25, 43, "PM");
+    cc.display();
+    sc.display();
+    mc.display();
+
+    return 0;
+}
