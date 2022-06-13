03df1792dd89bd890a8ff71c3a55b6b7a4da48b7
Chien <100185381+b1029009Chien@users.noreply.github.com>
2022-06-14 00:29:33 +0800

    Update main.cpp

--- hw9/main.cpp
+++ hw9/main.cpp
@@ -8,31 +8,28 @@ class ReplaceMystring {
 		ofstream out;
 		ifstream in;
 		string str;
-
+        int i = 0, j, k;
 		void replaceString(string str1, string str2) {
 			out.open("Rmain.cpp");
 			in.open("main.cpp");
 			while(getline(in, str)) {
-				int str1_position = str.find(str1, 0);
-				int str2_position = str.find(str2, 0);
-				if(str1_position != -1 && str2_position != -1) {
-					int i = 0, j, k;
+				int str1_pos = str.find(str1);
+				int str2_pos = str.find(str2);
+				if(str1_pos != string::npos && str2_pos != string::npos) {
 					int len = str.length();
 					int len1 = str1.length();
 					int len2 = str2.length();
-					int start = str2.find(str1, 0);
-
+					int start = str2.find(str1);
 					string str_front, str_behind;
 					str1 = str2;
 					str2 = str2.assign(str2, 0, start - 1);
 					str_behind = str_behind.assign(str2, start + len1, len2 - len1);
 					str2 += str_behind;
-
-					for(i = 0; i < str1_position; i++) {
+					for(i = 0; i < str1_pos; i++) {
 						out << str[i];
 					}
 					out << str1;
-					for(j = i + len1; j < str2_position; j++) {
+					for(j = i + len1; j < str2_pos; j++) {
 						out << str[j];
 					}
 					out << str2;
@@ -49,9 +46,8 @@ class ReplaceMystring {
 		}
 };
 
-int main(void) {
+int main() {
 	ReplaceMystring my;
 	my.replaceString("IU", "IU is best");
 	return 0;
 }
-
